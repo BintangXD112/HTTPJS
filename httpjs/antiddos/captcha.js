bad6f33d083e88319d1ccaeb74aaa2f6eaffaa6c
BintangXD112 <103170567+BintangXD112@users.noreply.github.com>
2025-07-03 08:39:03 +0700

    update captcha challenge code

--- httpjs/antiddos/captcha.js
+++ httpjs/antiddos/captcha.js
@@ -1,7 +1,17 @@
 // CAPTCHA Middleware
+const fs = require('fs');
+const path = require('path');
+const captchaPath = path.join(__dirname, '../view/captcha.html');
+
 module.exports = function captchaMiddleware(options = {}) {
   return (req, res, next) => {
-    // TODO: Implementasi CAPTCHA
+    if (req._captchaRequired) {
+      fs.readFile(captchaPath, (err, html) => {
+        res.writeHead(403, { 'Content-Type': 'text/html' });
+        res.end(html);
+      });
+      return;
+    }
     next();
   };
 }; 
